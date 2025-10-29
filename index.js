729e9ccb276c45ef09d633ec4935676d7d6c0eb7
Matheus <mdap.lic20@uea.edu.br>
2025-10-29 16:45:09 -0400

    fix(csp): adicionar Google Fonts ao CSP (style-src e font-src)

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Headers de segurança (relaxar CSP para permitir scripts inline necessários)
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self'");
+    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self'");
     next();
 });
 
